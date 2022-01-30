65dcf7c5da5af5dec9643622cb905dde5021fbdb
venesa okuna <venesa.okuna@student.moringaschool.com>
2022-01-30 21:49:04 +0300

    this commit will configure request

--- app/requests.py
+++ app/requests.py
@@ -10,3 +10,10 @@ base_url_source = None
 
 # getting news articles base url from the source id
 base_url_articles = None
+
+
+def configure_request(app):
+    global api_key, base_url_source, base_url_articles
+    base_url_source = app.config['NEWS_API_BASE_URL']
+    base_url_articles = app.config['NEWS_API_ARTICLES_URL']
+    api_key = app.config['NEWS_API_KEY']
